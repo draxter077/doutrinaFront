b5143e00fdb1a937e387d6047038985050f865af
Philippe <philippe.idalgoprestes@gmail.com>
2024-08-21 15:23:27 -0300

    new logo and colors

--- pages/init/body/main/logo/main.js
+++ pages/init/body/main/logo/main.js
@@ -1,6 +1,7 @@
 export default function logo(){
-    const logo = document.createElement("div")
+    const logo = document.createElement("img")
     logo.className = "initBodyLogo"
+    logo.src = "../../../../../logo.png"
     logo.innerHTML = "doutrina.net"
     return(logo)
 }
--- pages/read/main.js
+++ pages/read/main.js
@@ -1,6 +1,6 @@
 export default function read(n){
     const read = document.createElement("div")
-    console.log(`openning read ${n}`)
+    read.innerHTML = `openning read ${n}`
     //article.appendChild(top())
     //article.appendChild(body())
     //article.appendChild(bottom())
