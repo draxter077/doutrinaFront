3056ebb2057de5fafdf3d2234ba013adcdcaa138
Philippe <philippe.idalgoprestes@gmail.com>
2024-08-30 16:22:20 -0300

    m

--- pages/read/main.js
+++ pages/read/main.js
@@ -4,6 +4,7 @@ import bottom from "./bottom/main.js"
 
 export default function read(n){
     const read = document.createElement("div")
+    read.className = "read"
     read.appendChild(top())
     read.appendChild(body(n))
     read.appendChild(bottom())
