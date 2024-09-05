a521764fcf1cbdc151bbae355768166db1611be9
Philippe <philippe.idalgoprestes@gmail.com>
2024-09-05 15:04:03 -0300

    adminBodyStatsMostRead

--- pages/admin/body/stats/mostRead/list/option/main.js
+++ pages/admin/body/stats/mostRead/list/option/main.js
@@ -1,5 +1,12 @@
+import title from "./title/main.js"
+import author from "./author/main.js"
+//import views from "./views/main.js"
+
 export default function option(d){
     const option = document.createElement("div")
-    option.innerHTML = "oi"
+    option.className = "adminBodyStatsMostReadOption"
+    option.appendChild(title())
+    option.appendChild(author())
+    //option.appendChild(views())
     return(option)
 }
