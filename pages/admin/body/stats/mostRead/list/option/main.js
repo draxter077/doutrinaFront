77123251c624770ad81dcf59ee6e4feac002e39a
Philippe <philippe.idalgoprestes@gmail.com>
2024-09-05 16:53:09 -0300

    admin

--- pages/admin/body/stats/mostRead/list/option/main.js
+++ pages/admin/body/stats/mostRead/list/option/main.js
@@ -1,12 +1,12 @@
 import title from "./title/main.js"
 import author from "./author/main.js"
-//import views from "./views/main.js"
+import views from "./views/main.js"
 
-export default function option(d){
+export default function option(){
     const option = document.createElement("div")
     option.className = "adminBodyStatsMostReadOption"
     option.appendChild(title())
     option.appendChild(author())
-    //option.appendChild(views())
+    option.appendChild(views())
     return(option)
 }
---
+++ pages/admin/body/stats/mostRead/list/option/views/main.js
@@ -0,0 +1,5 @@
+export default function views(){
+    const views = document.createElement("div")
+    views.innerHTML = "1.234vs"
+    return(views)
+}
